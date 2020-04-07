fd4accdfde242269d9594546cfd581072c1cecd1
hernanruiz <ruizhernan93@gmail.com>
2020-04-07 19:45:26 -0300

    firs commit

---
+++ pruebaUno.php
@@ -0,0 +1,42 @@
+<?php
+/*$num1 = 1;
+$num4 = 1;
+$num3 = 1;
+$num2 = 1;
+$total=$num1+$num2+$num3+$num4;
+echo $total;
+*/
+
+/*$palabra = "palabra";
+$palabra = substr("palabra", 1);
+echo $palabra;*/
+
+/*$arreglo = [1, 2, 3, 4, 5, 6, 7, 8 , 9, 10];
+$total=0;
+for ($x=0; $x<10; $x++ ){
+    $total=$total+$arreglo[$x];    
+}
+ echo $total;
+*/
+ 
+
+/*$numbers = [1,2,3,4,5,6,7,8,9,10];
+
+
+echo $sum = array_sum ($numbers );*/
+
+
+ $a = 10;
+ $b = 12;
+ if ($a==$b){
+     echo $a+$b;
+
+ }
+ else{
+     $div= $a/$b;
+     $mult= $a*$b;
+     echo $div;
+     echo $mult;
+ }
+ 
+ ?>
